65fe46e8dfd3a456f8bf3b2af408edd7c7eda995
adamkjelstroem <adamkjelstroem@gmail.com>
2021-05-12 17:33:19 +0200

    minor cleanup

--- Alias_Analysis/src/graph/graph.cpp
+++ Alias_Analysis/src/graph/graph.cpp
@@ -22,8 +22,8 @@ void graph::construct2(string infile_name){
 			split(tokens[1], "[label=\"", tokens2);
 			string b = tokens2[0];
 			string label = tokens2[1];
-			
-			//parse 
+
+			//parse
 			//note the 'flipping' s.t. we "add an edge" from a to b by calling in order (b, a, label)
 			if (label.find("op") != string::npos){
 				addedge(getVertex(b), getVertex(a), getfield("("));
@@ -35,25 +35,26 @@ void graph::construct2(string infile_name){
 				addedge(getVertex(a), getVertex(b), getfield("["));
 			}else{
 				addedge(getVertex(b), getVertex(a), EPS);
-			} 
+			}
 			//cout<<"adding edge from "<<a<<" to "<<b<<" with label= "<<label<<endl;
 		}
 	}
 	dsu.init(vertices.size());
 }
 
+//performs graph flattening on brackets.
+//causes Theta(n^2) expansion of graph size
 graph graph::flattenbracket(){
 	graph g;
 
 	int n = vertices.size();
 	int c = n*n*12+n*6; //expansion factor.
-	c = 2;
-	for (int i = 0; i < c; i++){ //TODO <= c??
-		//TODO build graph
+	c = 2; //TODO shrink graph for testing purposes
+	for (int i = 0; i < c; i++){ 
 		for (auto vertex : vertices){
-			auto fit = vertex->edgesbegin();   
+			auto fit = vertex->edgesbegin();
 			while(fit!=vertex->edgesend()){   // iterating over field
-				field f = fit->first; 
+				field f = fit->first;
 				//f.field_name is edge label name
 				auto fedgeit = vertex->edgesbegin(f);
 				while(fedgeit != vertex->edgesend(f)){   // iterating over edges
@@ -110,8 +111,8 @@ void graph::construct2flattenbracket(string infile_name){
 			split(tokens[1], "[label=\"", tokens2);
 			string b = tokens2[0];
 			string label = tokens2[1];
-			
-			//parse 
+
+			//parse
 			//cout<<"adding edge from "<<a<<" to "<<b<<" with label= "<<label<<endl;
 			//note that a and b are flipped. this makes the graph compatible with the rest of the code base
 			//quite bizarre.
@@ -137,13 +138,13 @@ void graph::construct2flattenbracket(string infile_name){
 				for(int i = 0; i <= c; i++){ //note the <= operator
 					addedge(getVertex(b + ": " + std::to_string(i)), getVertex(a + ": " + std::to_string(i)),EPS);
 				}
-			} 
+			}
 		}
 	}
 	dsu.init(vertices.size());
 }
 
-// takes the file name containing the edge information of the spg as arguement 
+// takes the file name containing the edge information of the spg as arguement
 // and construct a Ngraph from it
 void graph::construct(string infile_name){
 	ifstream infile(infile_name);
@@ -157,7 +158,7 @@ void graph::construct(string infile_name){
 			// assert(tokens.size()==4);
 			addedge(getVertex(tokens[1]),getVertex(tokens[2]),getfield(tokens[3]));
 			// if(getfield(tokens[3])==EPS){
-			// 	addedge(getVertex(tokens[2]),getVertex(tokens[1]),EPS);	
+			// 	addedge(getVertex(tokens[2]),getVertex(tokens[1]),EPS);
 			// }
 			continue;
 		}
@@ -201,14 +202,14 @@ void graph::initWorklist(){
 		auto it = vtx->edgesbegin(); //pointer to pair<field,list<int>>
 		while(it!=vtx->edgesend()){
 			removeRepeatedges(it->second);
-			if(it->second.size()>=2) 
+			if(it->second.size()>=2)
 				worklist.push(pair<int,field>(vtx->id,it->first));
 			it++;
 		}
 	}
 }
 
-// algorithm described in this paper 
+// algorithm described in this paper
 void graph::bidirectedReach(){
 	//precondition graph::initWorklist() has been called
 	while(!worklist.empty()){
@@ -253,7 +254,7 @@ void graph::dsuUnion(int rootA,std::set<int>& roots){
 int graph::getRoot(Vertex* vtx,field& f,std::set<int>& roots,int& rootS){
 	auto it = vtx->edgesbegin(f); //pointer to vertex id (int)
 	// auto end = vtx->edgesend(f);
-	int rootA = -1; //invariant: rootA = max rank root so far excluding vtx 
+	int rootA = -1; //invariant: rootA = max rank root so far excluding vtx
 	int rankA = 0;
 	int rankS = 0;
 	while(it!=vtx->edgesend(f)){  //merge
@@ -261,7 +262,7 @@ int graph::getRoot(Vertex* vtx,field& f,std::set<int>& roots,int& rootS){
 		int rankB = dsu.rank(rootB);
 		if(rankB>rankS){
 			if(rankB>rankA){
-				rootS=rootA; rankS=rankA; 
+				rootS=rootA; rankS=rankA;
 				rootA=rootB; rankA=rankB;
 			}else if(rootA!=rootB){
 				rootS=rootB; rankS=rankB;
@@ -275,7 +276,7 @@ int graph::getRoot(Vertex* vtx,field& f,std::set<int>& roots,int& rootS){
 
 // append all the edges in roots to rootvtx
 void graph::appendedges(std::set<int>& roots,Vertex *rootvtx, int uid,field mf){
-	for(auto i=roots.begin();i!=roots.end();i++){ 
+	for(auto i=roots.begin();i!=roots.end();i++){
 		Vertex* rvtx = vertices[*i];
 		if(rvtx->id == rootvtx->id)
 			continue;
@@ -295,7 +296,7 @@ void graph::appendedges(std::set<int>& roots,Vertex *rootvtx, int uid,field mf){
 	}
 }
 
-// add <rootvertex,field> to worklist if possible  
+// add <rootvertex,field> to worklist if possible
 void graph::addnewPair(Vertex* rootvtx){
 	//worklist add. scope for optimisation
 	auto it = rootvtx->edgesbegin();
@@ -309,11 +310,11 @@ void graph::addnewPair(Vertex* rootvtx){
 
 
 void graph::removeRepeatedges(list<int> &l){
-	set<int> elem; 
+	set<int> elem;
 	auto it = l.begin();
 	while(it!=l.end()){
 		if( elem.find(dsu.root(*it))!=elem.end() ){
-			it = l.erase(it); 
+			it = l.erase(it);
 		}else{
 			elem.insert(*it = dsu.root(*it));
 			it++;
@@ -338,7 +339,7 @@ Vertex* graph::getVertex(const string &s){
 		str2vtx[s]=vtx;
 		return vtx;
 	}
-	return it->second; 
+	return it->second;
 }
 
 field& graph::getfield(const string &s){
@@ -354,11 +355,11 @@ field& graph::getfield(const string &s){
 
 void graph::addedge(Vertex* u,Vertex* v,field &f){
 	u->addedge(f,v->id);
-	if(u != v) 
+	if(u != v)
 		cout<<"("<<v->name<<") -> ("<<u->name<<") with label "<<f.field_name<<endl;
 	// if(f==EPS)
 	// 	numedges+=0.5;
-	// else 
+	// else
 		numedges++;
 }
 
@@ -430,9 +431,9 @@ void graph::printGraph(){
 
 
 void graph::printEdges(Vertex *vtx){
-	auto fit = vtx->edgesbegin();   
+	auto fit = vtx->edgesbegin();
 	while(fit!=vtx->edgesend()){   // iterating over field
-		field f = fit->first; 
+		field f = fit->first;
 		cout<<"** "<<f.field_name<<endl;
 		auto fedgeit = vtx->edgesbegin(f);
 		while(fedgeit != vtx->edgesend(f)){   // iterating over edges
