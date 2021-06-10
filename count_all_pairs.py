790fe96504292e20de63db010c62c7f30540cf15
adamkjelstroem <adamkjelstroem@gmail.com>
2021-06-10 16:47:03 +0200

    Update count_all_pairs.py

--- count_all_pairs.py
+++ count_all_pairs.py
@@ -1,8 +1,7 @@
 import sys, queue
 
 
-nodes = set()
-vertices = set()
+graph = dict()
 q = queue.Queue()
 
 #TODO to make this work, just keep 'out-edges' for a node. If it has two out-edges labeled "(" (to node a and node b), then there's an epsilon between a and b.
@@ -25,58 +24,48 @@ with open(sys.argv[1], 'r') as f:
             b = int(t[0])
             t = t[1].split("\"")[0].split("--")
             label = t[0]
-            v = ""
-            v2 = ""
-            if label == "ob":
-                v =  (a, b, "[")
-                v2 = (b, a, "]")
-            elif label == "cb":
-                v =  (b, a, "[")
-                v2 = (a, b, "]")
-            elif label == "op":
-                v =  (a, b, "(")
-                v2 = (b, a, ")")
-            elif label == "cp":
-                v =  (b, a, "(")
-                v2 = (a, b, ")")
 
-            vertices.add(v)
-            q.put(v)
-            vertices.add(v2)
-            q.put(v2)
+            if not a in graph:
+                graph.put(a, set())
+            if not b in graph:
+                graph.put(b, set())
+
+            if label == "ob":
+                graph[b].add((a, "["))
+                q.put((b, a, "["))
+            elif label == "cb":
+                graph[a].add((b, "["))
+                q.put((a, b, "["))
+            elif label == "op":
+                graph[b].add((a, "("))
+                q.put((b, a, "("))
+            elif label == "cp":
+                graph[a].add((b, "("))
+                q.put((a, b, "("))
+
 
             id = t[1]
 
-            nodes.add(a)
-            nodes.add(b)
 
-
-v2 = set()
-
-def push(v):
-    if not v in vertices:
-        v2.add(v)
-        q.put(v)
+def push(a, b, label):
+    if not (b, label) in graph[a]:
+        graph[a].add((b,label))
+        q.put((a, b, label))
 
 # Do parsing
 while not q.empty():
-    v = q.get()
-    for w in vertices:
-        if v[1] == w[0]:
-            # order is v w
-            if (v[2] == "(" and w[2] == ")") or (v[2] == "[" and w[2] == "]"):
-                push((v[0], w[1], ""))
-            elif v[2] == "":
-                push((v[0], w[1], w[2]))
-        elif w[1] == v[0]:
-            # order is w v
-            if (w[2] == "(" and v[2] == ")") or (w[2] == "[" and v[2] == "]"):
-                push((w[0], v[1], ""))
-            elif v[2] == "":
-                push((w[0], v[1], w[2]))
+    (a, b, label) = q.get()
 
-    for x in v2:
-        vertices.add(x)
+    vertices = graph[a]
+
+    if label=="":
+        for (c, label2) in vertices:
+            push((a, c, label2))
+    else:
+        for (c, label2) in vertices:
+            if label == label2:
+                push((a, c, ""))
+                push((c, a, ""))
 
     if q.qsize() % 1000 == 0:
         print(q.qsize())
@@ -84,9 +73,9 @@ while not q.empty():
 
 res = 0
 
-for (a, b, label) in vertices:
-    if a < b and label == "":
-        res+=1
+for a in graph.keys():
+    for (b, label) in graph[a]:
+        if a < b and label == "":
+            res += 1
 print(res)
-
 #2923 is res for "antlr_reduced"
