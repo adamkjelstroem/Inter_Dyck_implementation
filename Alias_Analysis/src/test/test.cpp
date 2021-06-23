f504c4d8eee62c6563c4f154082ff6c5e1f3e4c2
adamkjelstroem <adamkjelstroem@gmail.com>
2021-06-23 10:29:34 +0200

    refactor test

--- Alias_Analysis/src/test/test.cpp
+++ Alias_Analysis/src/test/test.cpp
@@ -442,31 +442,3 @@ bool Test::test(){
     return true;
 }
 
-
-
-graph buildSimple(int k){
-    graph g;
-    
-    for(int i = 0; i < k; i++){
-        g.getVertex(i, 0, "");
-        g.addEdge(i, 0, i+1, 0, "(");
-    }
-    for(int i = 0; i < k; i++){
-        g.getVertex((i+k), 0, "");
-        g.addEdge((i+k), 0, (i+k+1), 0, "[");
-    }
-    for(int i = 0; i < k; i++){
-        g.getVertex((i+k*2), 0, "");
-        g.addEdge((i+k*2+1), 0, (i+k*2), 0, "(");
-    }
-    for(int i = 0; i < k; i++){
-        g.getVertex((i+k*3), 0, "");
-        g.addEdge((i+k*3+1), 0, (i+k*3), 0, "[");
-    }
-
-    g.dsu.init(g.vertices.size());
-    g.initWorklist();
-        
-    
-    return g;  
-}
