1361a48a160b2eb0bfb33538cf3512e95d5e1795
adamkjelstroem <adamkjelstroem@gmail.com>
2021-07-08 22:47:34 +0200

    Revert "remove flattenReach (no longer needed)"
    This reverts commit f6cacd65e774611f90f187de580998a247a03afa.

--- Alias_Analysis/include/graph/graph.h
+++ Alias_Analysis/include/graph/graph.h
@@ -76,6 +76,8 @@ public:
 	void constructFromDot(string infile_name, bool d1_parenthesis, bool d1_bracket);
 	
 	graph flatten(string field_name, int depth);
+
+	void flattenReach(string flatten_label);
 	
 	int calcNumReachablePairs();
 
