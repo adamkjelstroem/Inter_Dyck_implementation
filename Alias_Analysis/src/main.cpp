ac73470ce8b3fc966375111e0c5a66f9aa6733f0
adamkjelstroem <adamkjelstroem@gmail.com>
2021-07-08 22:46:54 +0200

    Revert "code cloeanup"
    This reverts commit f20c3977917ab9fb0a477230b7283e21392be44f.

--- Alias_Analysis/src/main.cpp
+++ Alias_Analysis/src/main.cpp
@@ -1,4 +1,5 @@
 #include "graph/graph.h"
+#include "test/test.h"
 #include "graph/Ngraph.h"
 #include <iostream>
 #include <time.h>
@@ -15,18 +16,20 @@ void d1dk_experiment(string s, string counterSymbol){
 	g_copy.initWorklist();
 	g_copy.bidirectedReach();
 	int d_sccs = g_copy.computeSCCs().size();
+	int d_reachable_pairs = g_copy.calcNumReachablePairs();
 
 	clock_t t = clock();	
 	
 	g.bidirectedInterleavedDkD1Reach(s);
 
 	int id_sccs = g.computeSCCs().size();
+	int id_reachable_pairs = g.calcNumReachablePairs();
 
 	g.deleteVertices();
 
 	float time = ((float)clock()-t)/CLOCKS_PER_SEC;
 
-	cout<<s<<" & "<<n<<" & "<<id_sccs<<" & "<<d_sccs<<" & "<<time<<" \\\\ \\hline"<<endl;
+	cout<<s<<" & "<<n<<" & "<<id_sccs<<" & "<<id_reachable_pairs<<" & "<<d_sccs<<" & "<<d_reachable_pairs<<" & "<<time<<" \\\\ \\hline"<<endl;
 }
 
 set<pair<int, int>> getReachablePairs(graph& g){
@@ -331,7 +334,7 @@ int main(int argc, const char * argv[]){
 			
 
 			//use various rules of trimming to reduce graph
-			g_working = g_working.trim_d1d1(g_working);
+			g_working = g_working.trim(g_working);
 			cout<<endl;
 			cout<<"Graph after trimming:"<<endl;
 			g_working.printSparsenessFacts();
@@ -1087,7 +1090,24 @@ int main(int argc, const char * argv[]){
 		return 0;
 	}
 
-	
+	//tests flattenReach w/ memory
+	if(false){
+		//TODO broken??
+		graph g;
+		string s = 
+			"antlr";
+		string s2 = "./spg/reduced_bench/" + s + "_reduced.dot";
+
+		g.constructFromDot(s2, true, true);
+
+		g.flattenReach("[");
+
+		
+		cout<<"d1 dot d1 reachability for reduced version of "<<s<<": "<<g.calcNumReachablePairs()<<endl;
+
+		return 0;
+	}
+
 	if(true){
 		//tests early stopping by 'repeating scc patterns'
 
@@ -1183,6 +1203,256 @@ int main(int argc, const char * argv[]){
 		return 0;
 	}
 
+	if(false){
+		Test t;
+		graph g = t.buildSimple(3);
+
+		string flatten_label = "[";
+
+		graph flattened = g.flatten(flatten_label, 2);
+
+		flattened.printGraphAsTikz();
+
+		flattened.bidirectedReach();
+
+		flattened.printDetailReach();
+
+
+		auto addLayer = [](Vertex a, Vertex b, field f, void* extra[]) {
+				//TODO possibly move this definition outside the loop
+				graph* flattened = (graph*)extra[0];
+				int layer = *((int*)extra[1]);
+				string field_name = *(string*)extra[2];
+
+				if(f.field_name == field_name){ //TODO cache id of "[" field and do comparison on
+					//flatten on this field
+					flattened->addEdge(
+						a.x, layer-1, 
+						b.x, layer, 
+						"eps"
+					);
+				}else{
+					flattened->addEdge(
+						a.x, layer,
+						b.x, layer,
+						f.field_name
+					);
+						
+				}
+			};
+		
+		int i = 2;
+		void* w[] = {&flattened, &i, &flatten_label};
+		g.iterateOverEdges(addLayer, w);
+
+		flattened.dsu.init(flattened.N); 
+
+		flattened.initWorklist();
+
+		flattened.printGraphAsTikz();
+
+
+
+		flattened.bidirectedReach();
+
+		flattened.printDetailReach();
+
+		{
+			i = 3;
+			void* w[] = {&flattened, &i, &flatten_label};
+			g.iterateOverEdges(addLayer, w);
+		}
+
+		flattened.dsu.init(flattened.N); 
+
+		flattened.initWorklist();
+
+		flattened.printGraphAsTikz();
+
+
+		flattened.bidirectedReach();
+
+		flattened.printDetailReach();
+
+		return 0;
+	}
+
+
+
+	if(true){
+		string data[] = {
+			"antlr",
+			"bloat",
+			"chart",
+			"eclipse",
+			"fop",
+			"hsqldb",
+			"jython",
+			"luindex",
+			"lusearch",
+			"pmd",
+			"xalan"
+		};
+
+		for(string s : data){
+			string s2 = "./spg/reduced_bench/" + s + "_reduced.dot";
+			graph g;
+			
+			g.constructFromDot(s2, true, true); //Parses files in the ".dot" format as D1 dot D1
+			long long n = g.N;
+			long long c = n*n*18 + 6*n;
+			cout<<"Nodes of graph "<<s<<"_reduced: "<<n<<". This yields "<<c<<" layers with a total of "<<c*n<<" nodes when flattened."<<endl;
+		}
+
+		return 0;
+	}
+
+	if(false){
+		string data[] = {
+			"antlr",
+			"bloat",
+			"chart",
+			"eclipse",
+			"fop",
+			"hsqldb",
+			"jython",
+			"luindex",
+			"lusearch",
+			"pmd",
+			"xalan"
+		};
+		string flatten_on = "[";
+		for(string s : data){
+			string s2 = "./spg/reduced_bench/" + s + "_reduced.dot";
+			graph g;
+			
+			g.constructFromDot(s2, true, true); //Parses files in the ".dot" format as D1 dot D1
+			//g.flattenReachRemade(flatten_on);
+			
+			int height = (int)(((long long) 1000*1000*5) / ((long long)g.N));
+
+			cout<<"flattening graph "<<s<<" up to height "<<height<<endl;
+
+			graph h = g.flatten(flatten_on, height);
+
+			h.bidirectedReach();
+
+			cout<<"D1 dot D1 reachability for "<<s<<"_reduced (when flattening on '"<<flatten_on<<"' up to height "<<height<<"): "<<h.calcNumReachablePairs()<<endl;
+		}
+		return 0;
+	}
+
+	/*
+	if(true){
+		{
+			graph g;
+			g.construct2(argv[1]); //NOTE using construct2 uses the new format
+			g.initWorklist();
+			g.bidirectedReach();
+			cout<<"D' reachability: "<<g.calcNumReachablePairs()<<endl;
+		}
+		
+		graph g;
+		g.construct2(argv[1]); //NOTE using construct2 uses the new format
+		
+
+		for(int i = 0; i < 5; i++){
+			cout<<"doing iteration "<<(i+1)<<" of 10"<<endl;
+			g.flattenReach2("(");
+			cout<<"resulting number of pairs: "<<g.calcNumReachablePairs()<<endl;
+		}
+
+		return 0;
+	}*/
+
+	if(true){	
+		Test t;
+		bool res = t.test();
+
+		if(!res){
+			cout<<"A test failed!"<<endl;
+		}else{
+			cout<<"All tests passed!"<<endl;
+		}
+		return 0;
+	}	
+
+	while (false){
+
+		//compute number of reachable pairs for many graphs using
+		//1) flatten on parenthesis, then compute
+		//2) flatten on brackets, then compute
+		//3) flattenReach on parenthesis
+		//4) flattenReach on brackets
+		// if these don't all match, print the original graph
+		
+		graph original, g1, g2, g3, g4;
+
+		clock_t time;
+		struct timeval tv1;
+
+		gettimeofday(&tv1, NULL);
+		time = clock();
+		
+		original = Test::makeRandomGraph((int)tv1.tv_usec, 10, 7);
+
+		g1 = original.copy();
+		g2 = original.copy();
+		g3 = original.copy();
+		g4 = original.copy();
+
+		g1 = g1.flatten("(", 20);
+		g1.bidirectedReach();
+		int g1Pairs = g1.calcNumReachablePairs();
+		cout<<"g1 done"<<endl;
+
+		g2 = g2.flatten("[", 20);
+		g2.bidirectedReach();
+		int g2Pairs = g2.calcNumReachablePairs();
+		cout<<"g2 done"<<endl;
+
+		g3.flattenReach("(");
+		int g3Pairs = g3.calcNumReachablePairs();
+		cout<<"g3 done"<<endl;
+
+		g4.flattenReach("[");
+		int g4Pairs = g4.calcNumReachablePairs();
+		cout<<"g4 done"<<endl;
+
+		if(!(g1Pairs == g2Pairs && g2Pairs == g3Pairs && g3Pairs == g4Pairs)){
+
+
+			cout<<endl<<endl;
+
+			original.printGraphAsTikz();
+
+			cout<<endl<<endl;
+
+			cout<<"Reachability details for flattenReach on (:"<<endl;
+			g3.printDetailReach();
+
+			cout<<"Reachability details for flattenReach on [:"<<endl;
+			g4.printDetailReach();
+
+			graph g5 = original.copy();
+			g5.flattenReach("(");
+			graph g6 = original.copy();
+			g6.flattenReach("[");
+
+			cout<<"Found an exception example!"<<endl;
+			cout<<"flatten on (: "<<g1Pairs<<endl;
+			cout<<"flatten on [: "<<g2Pairs<<endl;
+			cout<<"flattenReach on (: "<<g3Pairs<<endl;
+			cout<<"flattenReach on [: "<<g4Pairs<<endl;
+			original.printAsDot();
+			
+
+			return 0;
+		}
+
+	}
+	
+	
 	// Bidirected Reach Algorithm
 	graph g2, g;
 	clock_t time;
