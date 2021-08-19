25e8ffeac161aa3749bc150fe74840c8b03fedc3
adamkjelstroem <adamkjelstroem@gmail.com>
2021-08-19 11:28:27 +0200

    cleanup

--- Alias_Analysis/spa/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "compiling the source code"
-mkdir -p bin
-javac -classpath ./lib/sootclasses-2.5.0.jar:./lib/jasminclasses-2.5.0.jar:./lib/polyglotclasses-1.3.5.jar -d ./bin `find ./src -name "*java"`
